eeba0d990e2cbb88bf2051731cc24342f02daec1
David Keller <mrcodeswildride@gmail.com>
2020-06-10 13:23:10 +0000

    es6 update

--- script.js
+++ script.js
@@ -1,112 +1,112 @@
-var moneyParagraph = document.getElementById("money");
-var pickNumberButton = document.getElementById("pickNumber");
-var numberParagraph = document.getElementById("number");
-var betParagraph = document.getElementById("bet");
-var betSlider = document.getElementById("betSlider");
-var higherPayoutParagraph = document.getElementById("higherPayout");
-var lowerPayoutParagraph = document.getElementById("lowerPayout");
-var higherButton = document.getElementById("higher");
-var lowerButton = document.getElementById("lower");
-var resultParagraph = document.getElementById("result");
-var playAgainButton = document.getElementById("playAgain");
+let moneyParagraph = document.getElementById(`moneyParagraph`)
+let numberButton = document.getElementById(`numberButton`)
+let numberParagraph = document.getElementById(`numberParagraph`)
+let betParagraph = document.getElementById(`betParagraph`)
+let betSlider = document.getElementById(`betSlider`)
+let higherPayoutParagraph = document.getElementById(`higherPayoutParagraph`)
+let lowerPayoutParagraph = document.getElementById(`lowerPayoutParagraph`)
+let higherButton = document.getElementById(`higherButton`)
+let lowerButton = document.getElementById(`lowerButton`)
+let resultParagraph = document.getElementById(`resultParagraph`)
+let playAgainButton = document.getElementById(`playAgainButton`)
 
-var money = 100;
-var number;
-var bet = 1;
-var higherPayout;
-var lowerPayout;
+let money = 100
+let number
+let higherPayout
+let lowerPayout
 
-pickNumberButton.addEventListener("click", pickNumber);
-betSlider.addEventListener("input", changeBet);
-higherButton.addEventListener("click", chooseHigher);
-lowerButton.addEventListener("click", chooseLower);
-playAgainButton.addEventListener("click", playAgain);
+numberButton.addEventListener(`click`, pickNumber)
+betSlider.addEventListener(`input`, changeBet)
+higherButton.addEventListener(`click`, chooseHigher)
+lowerButton.addEventListener(`click`, chooseLower)
+playAgainButton.addEventListener(`click`, playAgain)
 
 function pickNumber() {
-    pickNumberButton.disabled = true;
+  numberButton.disabled = true
 
-    number = Math.floor(Math.random() * 100);
-    numberParagraph.innerHTML = "The number is " + number + ". Will the next number be higher or lower?";
+  number = Math.floor(Math.random() * 10)
+  numberParagraph.innerHTML = `The number is ${number}. Will the next number be higher or lower?`
 
-    betParagraph.style.display = "inline-block";
-    betSlider.style.display = "inline-block";
+  betParagraph.style.display = `inline-block`
+  betSlider.style.display = `inline-block`
 
-    changeBet();
+  changeBet()
 
-    higherButton.style.display = "inline";
-    lowerButton.style.display = "inline";
+  higherButton.style.display = `inline`
+  lowerButton.style.display = `inline`
 }
 
 function changeBet() {
-    bet = parseFloat(betSlider.value);
-    betParagraph.innerHTML = "Bet: $" + bet;
+  betParagraph.innerHTML = `Bet: $${betSlider.value}`
 
-    higherPayout = 100 / (99 - number) * bet;
-    higherPayoutParagraph.innerHTML = "Payout for higher: $" + higherPayout.toFixed(2);
+  higherPayout = 10 / (9 - number) * betSlider.value
+  higherPayoutParagraph.innerHTML = `Payout for higher: $${higherPayout.toFixed(2)}`
 
-    lowerPayout = 100 / number * bet;
-    lowerPayoutParagraph.innerHTML = "Payout for lower: $" + lowerPayout.toFixed(2);
+  lowerPayout = 10 / number * betSlider.value
+  lowerPayoutParagraph.innerHTML = `Payout for lower: $${lowerPayout.toFixed(2)}`
 }
 
 function chooseHigher() {
-    betSlider.disabled = true;
-    higherButton.disabled = true;
-    lowerButton.disabled = true;
+  betSlider.disabled = true
+  higherButton.disabled = true
+  lowerButton.disabled = true
 
-    var nextNumber = Math.floor(Math.random() * 100);
+  let nextNumber = Math.floor(Math.random() * 10)
 
-    if (nextNumber > number) {
-        var amountWon = higherPayout - bet;
-        resultParagraph.innerHTML = "The next number is " + nextNumber + ". You won $" + amountWon.toFixed(2);
-        money = money + amountWon;
-    }
-    else {
-        resultParagraph.innerHTML = "The next number is " + nextNumber + ". You lost $" + bet;
-        money = money - bet;
-    }
+  if (nextNumber > number) {
+    let amountWon = higherPayout - betSlider.value
+    resultParagraph.innerHTML = `The next number is ${nextNumber}. You won $${amountWon.toFixed(2)}`
 
-    moneyParagraph.innerHTML = "Money: $" + money.toFixed(2);
-    playAgainButton.style.display = "inline";
+    money = money + amountWon
+  }
+  else {
+    resultParagraph.innerHTML = `The next number is ${nextNumber}. You lost $${betSlider.value}`
+    money = money - betSlider.value
+  }
+
+  moneyParagraph.innerHTML = `Money: $${money.toFixed(2)}`
+  playAgainButton.style.display = `inline`
 }
 
 function chooseLower() {
-    betSlider.disabled = true;
-    higherButton.disabled = true;
-    lowerButton.disabled = true;
+  betSlider.disabled = true
+  higherButton.disabled = true
+  lowerButton.disabled = true
 
-    var nextNumber = Math.floor(Math.random() * 100);
+  let nextNumber = Math.floor(Math.random() * 10)
 
-    if (nextNumber < number) {
-        var amountWon = lowerPayout - bet;
-        resultParagraph.innerHTML = "The next number is " + nextNumber + ". You won $" + amountWon.toFixed(2);
-        money = money + amountWon;
-    }
-    else {
-        resultParagraph.innerHTML = "The next number is " + nextNumber + ". You lost $" + bet;
-        money = money - bet;
-    }
+  if (nextNumber < number) {
+    let amountWon = lowerPayout - betSlider.value
+    resultParagraph.innerHTML = `The next number is ${nextNumber}. You won $${amountWon.toFixed(2)}`
 
-    moneyParagraph.innerHTML = "Money: $" + money.toFixed(2);
-    playAgainButton.style.display = "inline";
+    money = money + amountWon
+  }
+  else {
+    resultParagraph.innerHTML = `The next number is ${nextNumber}. You lost $${betSlider.value}`
+    money = money - betSlider.value
+  }
+
+  moneyParagraph.innerHTML = `Money: $${money.toFixed(2)}`
+  playAgainButton.style.display = `inline`
 }
 
 function playAgain() {
-    pickNumberButton.disabled = false;
-    numberParagraph.innerHTML = "";
+  numberButton.disabled = false
+  numberParagraph.innerHTML = ``
 
-    betParagraph.style.display = "none";
-    betSlider.style.display = "none";
-    betSlider.disabled = false;
+  betParagraph.style.display = `none`
+  betSlider.style.display = `none`
+  betSlider.disabled = false
 
-    higherPayoutParagraph.innerHTML = "";
-    lowerPayoutParagraph.innerHTML = "";
+  higherPayoutParagraph.innerHTML = ``
+  lowerPayoutParagraph.innerHTML = ``
 
-    higherButton.style.display = "none";
-    higherButton.disabled = false;
+  higherButton.style.display = `none`
+  higherButton.disabled = false
 
-    lowerButton.style.display = "none";
-    lowerButton.disabled = false;
+  lowerButton.style.display = `none`
+  lowerButton.disabled = false
 
-    resultParagraph.innerHTML = "";
-    playAgainButton.style.display = "none";
+  resultParagraph.innerHTML = ``
+  playAgainButton.style.display = `none`
 }
